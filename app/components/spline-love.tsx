4b714771a59ea7fad08ccef9c870b3e816dda32f
allofdaniel <allofdaniel1@gmail.com>
2024-10-06 11:12:29 +0900

    3d figure 검은색 해결

--- app/components/spline-love.tsx
+++ app/components/spline-love.tsx
@@ -1,12 +1,25 @@
 "use client";
 
-import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
 import { container, loading } from "./spline-love.css";
 import { Application } from "@splinetool/runtime";
-import { useState } from "react";
+import Spline from "@splinetool/react-spline";
 
 export default function SplineLove() {
+  const [isMobile, setIsMobile] = useState(false);
   const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // 모바일 기기 감지
+    const checkMobile = () => {
+      const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
+      setIsMobile(isMobileDevice);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   function onLoad(spline: Application) {
     if (spline) {
@@ -18,10 +31,19 @@ export default function SplineLove() {
   return (
     <div className={container}>
       {!loaded && <span className={loading}>불러오는 중</span>}
-      <Spline
-        scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode"
-        onLoad={onLoad}
-      />
+      {isMobile ? (
+        <img
+          src="https://pub-0bbf49e88dd74965a39e23f7f2cb868e.r2.dev/KakaoTalk_Photo_2024-10-06-00-20-21%20001.png"
+          alt="Wedding Invitation"
+          onLoad={() => setLoaded(true)}
+          style={{ display: loaded ? "block" : "none" }}
+        />
+      ) : (
+        <Spline
+          scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode"
+          onLoad={onLoad}
+        />
+      )}
     </div>
   );
 }
