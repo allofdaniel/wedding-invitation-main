a4a41c68cb9bb06193d1dfa9c6facae7a633ff06
allofdaniel <allofdaniel1@gmail.com>
2024-10-06 11:42:58 +0900

    모바일에서 사진 대체

--- app/components/spline-love.tsx
+++ app/components/spline-love.tsx
@@ -1,15 +1,15 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Spline from "@splinetool/react-spline";
 import { container, loading } from "./spline-love.css";
-import { useState, useEffect } from "react";
 
 export default function SplineLove() {
   const [loaded, setLoaded] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
+  // 모바일 기기 감지
   useEffect(() => {
-    // 모바일 기기 감지
     const checkMobile = () => {
       const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
       setIsMobile(isMobileDevice);
@@ -22,16 +22,25 @@ export default function SplineLove() {
 
   function onLoad() {
     setLoaded(true);
-    console.log("Spline loaded");
   }
 
   return (
     <div className={container}>
       {!loaded && <span className={loading}>불러오는 중</span>}
-      <Spline
-        scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode"
-        onLoad={onLoad}
-      />
+      {isMobile ? (
+        // 모바일에서는 이미지로 대체
+        <img
+          src="https://pub-0bbf49e88dd74965a39e23f7f2cb868e.r2.dev/KakaoTalk_Photo_2024-10-06-00-20-21%20001.png"
+          alt="대체 이미지"
+          style={{ width: "100%", height: "auto" }}
+        />
+      ) : (
+        // 데스크탑에서는 Spline 사용
+        <Spline
+          scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode"
+          onLoad={onLoad}
+        />
+      )}
     </div>
   );
 }
